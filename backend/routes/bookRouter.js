fa53acfa3afb60e3c3bcb0fb05384aea4206420e
NAVIN KUMAR <ck0424884@gmail.com>
2026-03-24 15:32:12 +0530

    Enable book router functionality by uncommenting code
    Uncommented the book router code to enable routing functionality.

--- backend/routes/bookRouter.js
+++ backend/routes/bookRouter.js
@@ -1,27 +1,3 @@
-// import {
-//   isAuthenticated,
-//   isAuthorized,
-// } from "../middlewares/authMiddleware.js";
-// import {
-//   addBook,
-//   deleteBook,
-//   getAllBooks,
-// } from "../controller/bookController.js";
-
-// import express from "express";
-
-// const router = express.Router();
-
-// router.post("/admin/add", isAuthenticated, isAuthorized("admin"), addBook);
-// router.get("/all", isAuthenticated, getAllBooks);
-// router.delete(
-//   "/delete/:id",
-//   isAuthenticated,
-//   isAuthorized("admin"),
-//   deleteBook
-// );
-
-// export default router;
 
 
 import express from "express";
