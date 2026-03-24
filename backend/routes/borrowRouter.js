c7c901be2d831b77ab76346837555e12572f0245
NAVIN KUMAR <ck0424884@gmail.com>
2026-03-24 15:32:27 +0530

    Clean up borrowRouter.js by removing comments
    Removed commented-out code for borrowRouter routes.

--- backend/routes/borrowRouter.js
+++ backend/routes/borrowRouter.js
@@ -1,39 +1,4 @@
-// import express from "express";
-// import {
-//   borrowedBooks,
-//   getBorrowedBooksForAdmin,
-//   recordBorrowedBook,
-//   returnBorrowBook,
-// } from "../controller/borrowController.js";
-// import {
-//   isAuthenticated,
-//   isAuthorized,
-// } from "../middlewares/authMiddleware.js";
 
-// const router = express.Router();
-
-// router.post(
-//   "/record-borrow-book/:id",
-//   isAuthenticated,
-//   isAuthorized("admin", "User"),
-//   recordBorrowedBook
-// );
-// router.get(
-//   "/borrowed-books-by-users",
-//   isAuthenticated,
-//   isAuthorized("Admin", "User"),
-//   getBorrowedBooksForAdmin
-// );
-// router.get("/my-borrowed-books", isAuthenticated, borrowedBooks);
-
-// router.put(
-//   "/return-borrowed-book/:bookId",
-//   isAuthenticated,
-//   isAuthorized("admin", "User"),
-//   returnBorrowBook
-// );
-
-// export default router;
 
 
 import express from "express";
