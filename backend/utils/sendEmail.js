c57509b6e2f65fd1e82c779416be29553485ffed
NAVIN KUMAR <ck0424884@gmail.com>
2026-03-24 15:35:39 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,25 +1,4 @@
-// import nodeMaller from "nodemailer";
 
-// export const sendEmail = async ({ email, subject, message }) => {
-//   const transporter = nodeMaller.createTransport({
-//     host: process.env.SMTP_HOST,
-//     service: process.env.SMTP_SERVICE,
-//     port: process.env.SMTP_PORT,
-//     auth: {
-//       user: process.env.SMTP_USER,
-//       pass: process.env.SMTP_PASSWORD,
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: process.env.SMTP_FROM_MAIL,
-//     to: email,
-//     subject,
-//     html: message,
-//   };
-
-//   await transporter.sendMail(mailOptions);
-// };
 
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
